5f20c58f310ad63cdf3bb0f1b59c9d32108879d2
marundhh <acquy2k5cute@gmail.com>
2025-03-24 11:17:34 +0700

    p

--- Assets/WeaponPositionLock.cs
+++ Assets/WeaponPositionLock.cs
@@ -1,21 +1,23 @@
 ﻿using UnityEngine;
+using System.Collections.Generic;
 
 public class WeaponPositionLock : MonoBehaviour
 {
-    private Vector3 initialPosition;
-    private Quaternion initialRotation;
+    [SerializeField] List<Transform> aimTargets; // Danh sách các Transform cần di chuyển
+    [SerializeField] float aimSmoothSpeed = 20;
+    [SerializeField] LayerMask aimMask;
 
-    void Start()
+    void Update()
     {
-        // Lưu vị trí ban đầu của vũ khí
-        initialPosition = transform.position;
-        initialRotation = transform.rotation;
-    }
+        Vector2 screenCenter = new Vector2(Screen.width / 2, Screen.height / 2);
+        Ray ray = Camera.main.ScreenPointToRay(screenCenter);
 
-    void LateUpdate()
-    {
-        // Giữ nguyên vị trí và hướng của vũ khí
-        transform.position = initialPosition;
-        transform.rotation = initialRotation;
+        if (Physics.Raycast(ray, out RaycastHit hit, Mathf.Infinity, aimMask))
+        {
+            foreach (Transform aimTarget in aimTargets)
+            {
+                aimTarget.position = Vector3.Lerp(aimTarget.position, hit.point, aimSmoothSpeed * Time.deltaTime);
+            }
+        }
     }
 }
