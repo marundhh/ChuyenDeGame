8f36138c857a6f86432b1c3d4a2b078f63332cfa
marundhh <acquy2k5cute@gmail.com>
2025-03-26 18:59:38 +0700

    p

---
+++ Assets/AmmoUI.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class AmmoUI : MonoBehaviour
+{
+    public RaycastWeapon weapon; // Tham chiếu đến vũ khí
+    public Text ammoText; // Nếu dùng UI Text
+    public TextMeshProUGUI ammoTMP; // Nếu dùng TextMeshPro
+
+    void Update()
+    {
+        if (weapon != null)
+        {
+            int ammoCount = weapon.GetAmmoCount(); // Lấy số lượng đạn hiện tại
+
+            if (ammoText != null)
+                ammoText.text = "Đạn: " + ammoCount.ToString();
+
+            if (ammoTMP != null)
+                ammoTMP.text = "Đạn: " + ammoCount.ToString();
+        }
+    }
+}
--- Assets/RayCastWeapon.cs
+++ Assets/RayCastWeapon.cs
@@ -1,5 +1,4 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using Fusion;
 
@@ -11,22 +10,49 @@ public class RaycastWeapon : NetworkBehaviour
     public TrailRenderer tracerEffect;
     public Transform raycastOrigin;
     public Transform raycastDestination;
+    public int magazineSize = 30; // Số đạn tối đa
+    public float reloadTime = 2f; // Thời gian chờ hồi đạn
+    public float fireRate = 0.2f; // Độ trễ giữa các phát bắn
+
+    private int currentAmmo;
+    private float nextFireTime;
+    private bool isReloading = false;
+
+    public AudioSource audioSource;
+    public AudioClip fireSound;
+    public AudioClip reloadSound;
+    public AudioClip emptyMagSound; // Âm thanh khi hết đạn
 
     Ray ray;
     RaycastHit hitInfo;
 
     public override void Spawned()
     {
-        // Kiểm tra xem player này có quyền bắn hay không
         if (!Object.HasInputAuthority) enabled = false;
+        currentAmmo = magazineSize; // Đạn đầy khi bắt đầu
     }
 
     public void StartFiring()
     {
-        if (!Object.HasInputAuthority) return; // Chỉ người chơi local mới có thể bắn
+        if (!Object.HasInputAuthority || isReloading) return;
+        if (Time.time < nextFireTime) return; // Giới hạn tốc độ bắn
 
-        isFiring = true;
-        RPC_FireWeapon(raycastOrigin.position, raycastDestination.position);
+        if (currentAmmo > 0)
+        {
+            isFiring = true;
+            currentAmmo--; // Giảm đạn
+            nextFireTime = Time.time + fireRate;
+
+            RPC_FireWeapon(raycastOrigin.position, raycastDestination.position);
+        }
+        else
+        {
+            if (!isReloading)
+            {
+                PlayEmptyMagSound(); // Chơi âm thanh hết đạn
+                StartCoroutine(ReloadAmmo()); // Hết đạn thì tự động nạp lại
+            }
+        }
     }
 
     public void StopFiring()
@@ -43,6 +69,11 @@ public class RaycastWeapon : NetworkBehaviour
             particle.Emit(1);
         }
 
+        if (audioSource && fireSound)
+        {
+            audioSource.PlayOneShot(fireSound);
+        }
+
         ray.origin = origin;
         ray.direction = destination - origin;
 
@@ -54,8 +85,34 @@ public class RaycastWeapon : NetworkBehaviour
             hitEffect.transform.position = hitInfo.point;
             hitEffect.transform.forward = hitInfo.normal;
             hitEffect.Emit(1);
-
             tracer.transform.position = hitInfo.point;
         }
+    }
+
+    private IEnumerator ReloadAmmo()
+    {
+        if (isReloading) yield break;
+
+        isReloading = true;
+        if (audioSource && reloadSound)
+        {
+            audioSource.PlayOneShot(reloadSound);
+        }
+        yield return new WaitForSeconds(reloadTime);
+        currentAmmo = magazineSize;
+        isReloading = false;
+    }
+
+    private void PlayEmptyMagSound()
+    {
+        if (audioSource && emptyMagSound)
+        {
+            audioSource.PlayOneShot(emptyMagSound);
+        }
+    }
+
+    public int GetAmmoCount()
+    {
+        return currentAmmo;
     }
 }
